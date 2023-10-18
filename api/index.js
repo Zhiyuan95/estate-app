117df5b6f6483cbb31c3728124da070d88a0f6dd
Zhiyuan Zhang <68951730+Zhiyuan95@users.noreply.github.com>
2023-10-18 14:27:56 +1000

    connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+//we store key in the .env as this file will be ignored while uploading to github or other public website
+//we need package dotenv to help us handle reading MONGO from .env file
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
