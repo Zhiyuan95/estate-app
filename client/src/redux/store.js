fab00389421a6a5998e648f61c4fceb6e0e4dca1
Zhiyuan Zhang <68951730+Zhiyuan95@users.noreply.github.com>
2023-10-20 11:05:02 +1000

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+/**
+ we import redux-persist to help us save data in the place like broswer storage just in case of we lost data
+ after refrshing pages
+ */
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   //by calling this function, we can ignore some warning when the state and action sent to store are not serializable
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
