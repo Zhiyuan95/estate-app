bc41ddeefd743ba9bc98ec237562ca5b96e96e5a
Zhiyuan Zhang <68951730+Zhiyuan95@users.noreply.github.com>
2023-10-27 11:30:48 +1000

    complete header search form

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //it retruns query part of URL, params after '?' in url
+    console.log("urlParams is:", urlParams);
+    urlParams.set("searchTerm", searchTerm);
+    navigate(`/search?${urlParams.toString()}`);
+  };
+
+  useEffect(() => {
+    setSearchTerm(urlParams.get("searchTerm" || ""));
+  }, [window.location.search]);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
@@ -13,14 +31,20 @@ const Header = () => {
             <span className="text-slate-500">Estate</span>
           </h1>
         </Link>
-
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-500" />
+          <button>
+            <FaSearch className="text-slate-500" />
+          </button>
         </form>
         <ul className="flex gap-4 ">
           <Link to="/home">
