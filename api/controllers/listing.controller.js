790ca6f7a0c3e15ff038fac66e43274c6649c9b2
Zhiyuan Zhang <68951730+Zhiyuan95@users.noreply.github.com>
2023-10-27 10:36:17 +1000

    complete search listings api router

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -58,3 +58,54 @@ export const getTheListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit || 9);
+    const startIndex = parseInt(req.query.startIndex || 0);
+
+    //if the user don't specify a value of the 'offer', or set it to false, the query will return all records where
+    //the 'offer' value is either true or false
+    let offer = req.query.offer;
+    //we want to list all properties with/without offer at the begining search
+    if (!offer || offer === "false") {
+      //find in DB where the offer is either true or false
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    if (!furnished || furnished === "false") {
+      furnished = { $in: [true, false] };
+    }
+
+    let parking = req.query.parking;
+    if (!parking || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (!type || type === "all") {
+      type = { $in: ["sell", "rent"] };
+    }
+
+    const serachTerm = req.query.serachTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      //fuzzy search and case insensitive
+      name: { $regex: serachTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listing.router.js
+++ api/routes/listing.router.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   creatListing,
   deleteListing,
+  getListings,
   getTheListing,
   updateListing,
 } from "../controllers/listing.controller.js";
@@ -13,5 +14,6 @@ router.post("/create", verifyToken, creatListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getTheListing);
+router.get("/get", getListings);
 
 export default router;
